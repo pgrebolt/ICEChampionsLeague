b3b304411a6dcef60c0ddee996bfa596815db963
PGT <paugrebol@gmail.com>
2025-04-23 15:39:28 +0200

    Added create_standings to pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -4,6 +4,7 @@ py download_results.py
 
 jupyter nbconvert --to notebook --execute --inplace create_stats.ipynb
 jupyter nbconvert --to notebook --execute --inplace create_teamfrequencies.ipynb
+jupyter nbconvert --to notebook --execute --inplace create_standings.ipynb
 jupyter nbconvert --to notebook --execute --inplace plot_stats.ipynb
 jupyter nbconvert --to notebook --execute --inplace plot_frequencies.ipynb
 
