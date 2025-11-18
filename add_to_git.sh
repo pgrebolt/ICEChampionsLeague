2da645bd46ced9c054c079f434c3459a6b755c03
PGT <paugrebol@gmail.com>
2025-11-18 15:36:28 +0100

    Solved path issue in add_to_git.sh

--- add_to_git.sh
+++ add_to_git.sh
@@ -2,16 +2,16 @@
 
 # Add to git
 git add results/ELO_stats.png
-git add results.csv
 git add results/frequencies.png
 git add results/standings.md
 git add results/winplayed_stats.html
 git add results/winplayed_stats.png
-git add stats.nc
-git add teammates.nc
+git add generated_files/results.csv
+git add generated_files/stats.nc
+git add generated_files/teammates.nc
 
 ## Extract matchday number
-file="results.csv"
+file="generated_files/results.csv"
 last_line=$(tail -n 1 "$file")
 matchday="${last_line:0:2}"
 echo "Last matchday number is $matchday"
