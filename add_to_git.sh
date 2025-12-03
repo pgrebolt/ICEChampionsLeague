9257eb9e4410c6d7a76498221eb6cc2ca50ab4c4
PGT <paugrebol@gmail.com>
2025-12-03 13:32:59 +0100

    Afegit els results de la season a add_to_git

--- add_to_git.sh
+++ add_to_git.sh
@@ -6,7 +6,7 @@ git add results/frequencies.png
 git add results/standings.md
 git add results/winplayed_stats.html
 git add results/winplayed_stats.png
-git add generated_files/results.csv
+git add generated_files/results_Season_*.csv
 git add generated_files/stats.nc
 git add generated_files/teammates.nc
 git add generated_files/results_historical.csv
