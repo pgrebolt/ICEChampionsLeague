dca6c17b4413d7faad8fb9a5a5773865905741b3
PGT <paugrebol@gmail.com>
2026-01-07 16:15:29 +0100

    Creat fitxer d'estadístiques després de cada partit

--- src/create_stats_match.py
+++ src/create_stats_match.py
@@ -100,6 +100,9 @@ plt.rcParams['axes.prop_cycle'] = plt.cycler(color=plt.cm.tab20.colors)
 
 # Funció per actualitzar l'ELO després de cada partit
 def update_elo(results_df, parameters):
+    # Condició de si és el primer partit
+    first_game = True if results_df.shape[0] == 1 else False
+
     # ELO inicial
     initial_ELO = 1000.
 
@@ -107,14 +110,14 @@ def update_elo(results_df, parameters):
     K = 30.
 
     # Paràmetres de ponderació
-    mu, lamb = 0.3, 0.7
+    mu, lamb = 0.7, 0.3
 
     #################
     # Pel primer partit, tots els jugadors tenen l'ELO inicial
-    if results_df.shape[0] == 1:  # pel primer partit
-        return ({players_names[player_index]: initial_ELO for player_index in range(len(players_names))},
-                {players_names[player_index]: initial_ELO for player_index in range(len(players_names))},
-                {players_names[player_index]: 1/len(players_names) for player_index in range(len(players_names))})
+    #if results_df.shape[0] == 1:  # pel primer partit
+    #    return ({players_names[player_index]: initial_ELO for player_index in range(len(players_names))},
+    #            {players_names[player_index]: initial_ELO for player_index in range(len(players_names))},
+    #            {players_names[player_index]: 1/len(players_names) for player_index in range(len(players_names))})
     #################
 
     # Obtenim els noms jugadors del partit actual (anotat com a [-1])
@@ -129,21 +132,31 @@ def update_elo(results_df, parameters):
     away_defender_index =  np.where(players_names == away_defender_1)[0][0]
     away_attacker_index =  np.where(players_names == away_attacker_1)[0][0]
 
-    # ELO (rating) actuals (el [-1] representa l'últim partit jugat, l'últim ELO)
-    R_home_defender = parameters['ELODefense'][-1][home_defender_index]
-    R_home_attacker = parameters['ELOAttack'][-1][home_attacker_index]
-    R_away_defender = parameters['ELODefense'][-1][away_defender_index]
-    R_away_attacker = parameters['ELOAttack'][-1][away_attacker_index]
+    if first_game: # pel primer partit, assignar els valors inicials
+        R_home_defender, R_home_attacker, R_away_defender, R_away_attacker = initial_ELO, initial_ELO, initial_ELO, initial_ELO
+        n_home_defender, n_home_attacker, n_away_defender, n_away_attacker = 0., 0., 0., 0.
+    else:
+        # ELO (rating) actuals (el [-1] representa l'últim partit jugat, l'últim ELO)
+        R_home_defender = parameters['ELODefense'][-1][home_defender_index]
+        R_home_attacker = parameters['ELOAttack'][-1][home_attacker_index]
+        R_away_defender = parameters['ELODefense'][-1][away_defender_index]
+        R_away_attacker = parameters['ELOAttack'][-1][away_attacker_index]
 
-    # Partits jugats per cada jugador
-    n_home_defender = parameters['PlayedDefense'][-1][home_defender_index]
-    n_home_attacker = parameters['PlayedAttack'][-1][home_attacker_index]
-    n_away_defender = parameters['PlayedDefense'][-1][away_defender_index]
-    n_away_attacker = parameters['PlayedAttack'][-1][away_attacker_index]
+        # Partits jugats per cada jugador
+        n_home_defender = parameters['PlayedDefense'][-1][home_defender_index]
+        n_home_attacker = parameters['PlayedAttack'][-1][home_attacker_index]
+        n_away_defender = parameters['PlayedDefense'][-1][away_defender_index]
+        n_away_attacker = parameters['PlayedAttack'][-1][away_attacker_index]
 
     # ELO mitjà per equip
-    E_home = (R_home_defender * n_home_defender + R_home_attacker * n_home_attacker) / (n_home_defender + n_home_attacker)
-    E_away = (R_away_defender * n_away_defender + R_away_attacker * n_away_attacker) / (n_away_defender + n_away_attacker)
+    if n_home_defender == 0. and n_home_attacker == 0.: # si és el primer partit en cada posició de tots dos jugadors, assigna ELO inicial (evita divisió per 0)
+        E_home = initial_ELO
+    else:
+        E_home = (R_home_defender * n_home_defender + R_home_attacker * n_home_attacker) / (n_home_defender + n_home_attacker)
+    if n_away_defender == 0. and n_away_attacker == 0.:
+        E_away = initial_ELO
+    else:
+        E_away = (R_away_defender * n_away_defender + R_away_attacker * n_away_attacker) / (n_away_defender + n_away_attacker)
 
     # Probabilitats de victòria segons ELO
     P_home = 1 / (1 + 10 ** ((E_away - E_home) / 400))
@@ -165,10 +178,10 @@ def update_elo(results_df, parameters):
     received_away = results_df.iloc[-1]['Local']
 
     # Rendiment sobre la contribució al partit segons posició
-    r_defender_home = lamb * (1 - received_home/3) + mu*(results_df.iloc[-1]['Gols 1']/3) # defensa
-    r_attacker_home = mu * (1 - received_home/3) + lamb*(results_df.iloc[-1]['Gols 2']/3) # atac
-    r_defender_away = lamb * (1 - received_away/3) + mu*(results_df.iloc[-1]['Gols 3']/3)  # defensa
-    r_attacker_away = mu * (1 - received_away/3) + lamb*(results_df.iloc[-1]['Gols 4']/3) # atac
+    r_defender_home = mu * (1 - received_home/3) + lamb*(results_df.iloc[-1]['Gols 1']/3) # defensa
+    r_attacker_home = lamb * (1 - received_home/3) + mu*(results_df.iloc[-1]['Gols 2']/3) # atac
+    r_defender_away = mu * (1 - received_away/3) + lamb*(results_df.iloc[-1]['Gols 3']/3)  # defensa
+    r_attacker_away = lamb * (1 - received_away/3) + mu*(results_df.iloc[-1]['Gols 4']/3) # atac
 
     # Imposem un rendiment mínim de 0.1
     r_defender_home = max(r_defender_home, 0.1)
@@ -195,34 +208,63 @@ def update_elo(results_df, parameters):
     R_home_attacker_new = R_home_attacker + K * (S_home - P_home) * r_attacker_home / r_home
     R_away_defender_new = R_away_defender + K * (S_away - P_away) * r_defender_away / r_away
     R_away_attacker_new = R_away_attacker + K * (S_away - P_away) * r_attacker_away / r_away
+    #print(R_away_defender, S_away, P_away, r_defender_away, r_attacker_away, r_away, R_away_defender_new, R_away_attacker_new)
 
     ## ELO ponderat
-    # Normalizem ELOs (normalització min-max)
-    defense_rng = max(parameters['ELODefense'][-1]) - min(parameters['ELODefense'][-1]) # denominador (max - min)
-    attack_rng = max(parameters['ELOAttack'][-1]) - min(parameters['ELOAttack'][-1])
-    defense_rng = defense_rng if defense_rng != 0 else 1  # Evitem divisió per 0
-    attack_rng = attack_rng if attack_rng != 0 else 1  # Evitem divisió per 0
-    normalized_ELO_defense = [(parameters['ELODefense'][-1][player_index] - min(parameters['ELODefense'][-1])) / defense_rng for player_index in range(len(players_names))]
-    normalized_ELO_attack = [(parameters['ELOAttack'][-1][player_index] - min(parameters['ELOAttack'][-1])) / attack_rng for player_index in range(len(players_names))]
+    if first_game:
+        # Si és el primer partit, assignem un ELO ponderat neutre (0.5)
+        weighted_ELO = {players_names[player_index]: 0.5 for player_index in range(len(players_names))}
+    else:
+        # Normalizem ELOs (normalització min-max)
+        defense_rng = max(parameters['ELODefense'][-1]) - min(parameters['ELODefense'][-1]) # denominador (max - min)
+        attack_rng = max(parameters['ELOAttack'][-1]) - min(parameters['ELOAttack'][-1])
+        defense_rng = defense_rng if defense_rng != 0 else 1  # Evitem divisió per 0
+        attack_rng = attack_rng if attack_rng != 0 else 1  # Evitem divisió per 0
+        normalized_ELO_defense = [(parameters['ELODefense'][-1][player_index] - min(parameters['ELODefense'][-1])) / defense_rng for player_index in range(len(players_names))]
+        normalized_ELO_attack = [(parameters['ELOAttack'][-1][player_index] - min(parameters['ELOAttack'][-1])) / attack_rng for player_index in range(len(players_names))]
+        if np.all(normalized_ELO_defense==np.float64(0.)): # pel primer partit, assignar manualment els pesos a 0.5
+            normalized_ELO_defense = 0.5*np.ones(len(players_names))
+        if np.all(normalized_ELO_attack == np.float64(0.)):
+            normalized_ELO_attack = 0.5*np.ones(len(players_names))
+        # Calculem el valor ponderat
+        weight_attack = np.divide(parameters['PlayedAttack'][-1], parameters['GamesPlayed'][-1],
+                                  out=np.zeros_like(parameters['PlayedAttack'][-1]), where=parameters['GamesPlayed'][-1]!=0)
+        weight_defense = np.divide(parameters['PlayedDefense'][-1], parameters['GamesPlayed'][-1],
+                                  out=np.zeros_like(parameters['PlayedDefense'][-1]),
+                                  where=parameters['GamesPlayed'][-1] != 0)
+    #    weight_attack = parameters['PlayedAttack'][-1] / parameters['GamesPlayed'][-1]
+        #weight_defense = parameters['PlayedDefense'][-1] / parameters['GamesPlayed'][-1]
+        #weight_attack = np.where(weight_attack==np.nan, 1., weight_attack)  # Evitem NaN (si no s'ha jugat cap partit encara)
+        #weight_defense = np.where(weight_defense==np.nan, 1., weight_defense)
 
-    # Calculem el valor ponderat
-    weight_attack = parameters['PlayedAttack'][-1] / parameters['GamesPlayed'][-1]
-    weight_defense = parameters['PlayedDefense'][-1] / parameters['GamesPlayed'][-1]
-    weight_attack = np.where(weight_attack==np.nan, 1., weight_attack)  # Evitem NaN (si no s'ha jugat cap partit encara)
-    weight_defense = np.where(weight_defense==np.nan, 1., weight_defense)
+        weighted_ELO = {players_names[player_index]: weight_defense[player_index] * normalized_ELO_defense[player_index] + weight_attack[player_index] * normalized_ELO_attack[player_index] for player_index in range(len(players_names))}
 
-    weighted_ELO = {players_names[player_index]: weight_defense * normalized_ELO_defense[player_index] + weight_attack * normalized_ELO_attack[player_index] for player_index in range(len(players_names))}
+    ## Retornem els nous ELOs
+    if first_game:
+        # Llista amb els ELO després del primer partit
+        first_elos_attack = initial_ELO * np.ones(len(players_names)) # llista amb ELO igual per tothom
+        first_elos_defense = initial_ELO * np.ones(len(players_names))
+        first_elos_defense[home_defender_index] = R_home_defender_new # actualitzem només els jugadors del partit
+        first_elos_attack[home_attacker_index] = R_home_attacker_new
+        first_elos_defense[away_defender_index] = R_away_defender_new
+        first_elos_attack[away_attacker_index] = R_away_attacker_new
 
-    # Actualitzem la matriu amb tots els ELO (només els jugadors del partit actual)
-    parameters['ELODefense'][-1][home_defender_index] = R_home_defender_new
-    parameters['ELOAttack'][-1][home_attacker_index] = R_home_attacker_new
-    parameters['ELODefense'][-1][away_defender_index] = R_away_defender_new
-    parameters['ELOAttack'][-1][away_attacker_index] = R_away_attacker_new
+        # Retorn en forma de diccionari
+        return ({players_names[player_index]: first_elos_defense[player_index] for player_index in range(len(players_names))},
+                {players_names[player_index]: first_elos_attack[player_index] for player_index in range(len(players_names))},
+                weighted_ELO)
 
-    # Tornem un diccionari per atac i un altre per defensa
-    return ({players_names[player_index]: parameters['ELODefense'][-1][player_index] for player_index in range(len(players_names))},
-            {players_names[player_index]: parameters['ELOAttack'][-1][player_index] for player_index in range(len(players_names))},
-            weighted_ELO)
+    else:
+        # Actualitzem la matriu amb tots els ELO (només els jugadors del partit actual)
+        parameters['ELODefense'][-1][home_defender_index] = R_home_defender_new
+        parameters['ELOAttack'][-1][home_attacker_index] = R_home_attacker_new
+        parameters['ELODefense'][-1][away_defender_index] = R_away_defender_new
+        parameters['ELOAttack'][-1][away_attacker_index] = R_away_attacker_new
+
+        # Tornem un diccionari per atac, un altre per defensa i un altre per weighted
+        return ({players_names[player_index]: parameters['ELODefense'][-1][player_index] for player_index in range(len(players_names))},
+                {players_names[player_index]: parameters['ELOAttack'][-1][player_index] for player_index in range(len(players_names))},
+                weighted_ELO)
 
 # Carreguem les dades
 season = 'historical' # 2,3, 4, historical
@@ -230,7 +272,7 @@ if season == 'historical':
     data_df = pd.read_csv(f'../generated_files/results_{season}.csv')
 else:
     data_df = pd.read_csv(f'../generated_files/results_Season_{season}.csv')
-data_df = pd.read_csv(f'../generated_files/results_prova.csv')
+#data_df = pd.read_csv(f'../generated_files/results_prova.csv')
 # Emplenem els espais en blanc amb 0
 data_df = data_df.fillna(0.)
 
@@ -292,10 +334,6 @@ played_j1 = {player: 0 for player in players_names}
 played_j2 = {player: 0 for player in players_names}
 played_j3 = {player: 0 for player in players_names}
 played_j4 = {player: 0 for player in players_names}
-
-#ELO rating inicial
-elo_rating_attack = {player: 1000. for player in players_names}
-elo_rating_defense = {player: 1000. for player in players_names}
 
 # Iterem per cada partit
 for match in range(1, matches+1):
@@ -377,8 +415,8 @@ for match in range(1, matches+1):
     ## Càlcul d'ELO actualitzat després d'aquest partit
     elo_defense_match, elo_attack_match, elo_weighted_match = update_elo(previous_matches, _acc)
 
-    print(elo_defense_match)
-    print(elo_weighted_match)
+    #print(elo_attack_match)
+    #print(elo_weighted_match)
 
     # Guanyats
     winattackplayed_match = {k: winattack_match.get(k, 0) / (playedattack_match.get(k, 1) if playedattack_match.get(k, 1) else 1) for k in players_names} # evitem divisió per 0
@@ -409,29 +447,36 @@ for match in range(1, matches+1):
     _acc["ReceivedDefensePlayed"].append(_dict_to_row(receiveddefenseplayed_match, players_names))
 
     # ELO dicts (ensure these are per-matchday snapshots or dictionaries of current ratings)
+    #TODO: no està creant bé els diccionaris. El primer partit no es desa i l'últim i el penúltim són iguals
+    #print(elo_attack_match)
+    #print(_acc["ELOAttack"])
     _acc["ELOAttack"].append(_dict_to_row(elo_attack_match, players_names))
+    #print(_acc["ELOAttack"], '\n')
     _acc["ELODefense"].append(_dict_to_row(elo_defense_match, players_names))
     _acc["WeightedELO"].append(_dict_to_row(elo_weighted_match, players_names))
 
-    # track matchday coordinate (use whatever label you want; here nmatchday+1)
+    # track matchday coordinate
     _match_coords.append(match)
 
     # Si es vol fer frequencies
     # previous_matches.groupby(['Jugador 1', 'Jugador 2'])['Jugador 1'].count() #això compta les vegades que hi ha hagut una parella
 
 ## Creem el dataset de xarray
+# Creem un diccionari de paràmetre (key) i (value->) tupla amb el nom de dimensions i una matriu numpy dels paràmetres. És la manera que a xarray li agrada
 _data_vars = {
     p: (("match", "player"), np.vstack(_acc[p]) if len(_acc[p]) > 0 else np.empty((0, len(players_names))))
     for p in params
 }
 
+# Creem el dataset a partir del diccionari
 ds = xr.Dataset(
     data_vars=_data_vars,
     coords={"match": _match_coords, "player": players_names}
 )
-print(ds)
-print(matches)
-print(ds['ReceivedAttack'].sel(match=matches, player='Pau').max())
+#print(ds)
+#print(matches)
+#print(_match_coords, 'aaaaa')
+#print(ds['ELOAttack'].sel(match=5))
 
 # Afegim el càlcul dels índexs d'atac i de defensa en base al paràmetres que ja hem calculat:
 #
@@ -444,28 +489,16 @@ print(ds['ReceivedAttack'].sel(match=matches, player='Pau').max())
 #dataset['AttackIndex'] = dataset['ScoredAttack'] * dataset['PlayedAttack'] / filtered_games_played + dataset['ScoredDefense'] * dataset['PlayedDefense'] / filtered_games_played
 #dataset['DefenseIndex'] = dataset['ReceivedAttack'] * dataset['PlayedAttack'] / filtered_games_played + dataset['ScoredDefense'] * dataset['PlayedDefense'] / filtered_games_played
 
-# Weighted ELO a partir del valors normalitzats min-max
-normalized_ELO_attack = (dataset['ELOAttack'] - dataset['ELOAttack'].min()) / (dataset['ELOAttack'].max() - dataset['ELOAttack'].min())
-normalized_ELO_defense = (dataset['ELODefense'] - dataset['ELODefense'].min()) / (dataset['ELODefense'].max() - dataset['ELODefense'].min())
-
-dataset['WeightedELO'] = normalized_ELO_attack * dataset['PlayedAttack'] / filtered_games_played + normalized_ELO_defense * dataset['PlayedDefense'] / filtered_games_played
-
-# Si algun jugador només ha jugat en una posició, pertorba la normalització min-max. Fem que el seu valor sigui nan
-#print(dataset['WeightedELO'].max(), dataset['WeightedELO'].min())
-dataset['WeightedELO'] = dataset['WeightedELO'].where(dataset['WeightedELO'] < 500)
-
 ## Desem el dataset a un fitxer netCDF
-#if season == 'historical':
-#    dataset.to_netcdf('../generated_files/stats_historical.nc', mode='w')
-#else:
-#    dataset.to_netcdf('../generated_files/stats.nc', mode='w')
+if season == 'historical':
+    ds.to_netcdf('../generated_files/stats_historical.nc', mode='w')
+else:
+    ds.to_netcdf('../generated_files/stats.nc', mode='w')
 
-# ds = xr.open_dataset('stats.nc', engine ='netcdf4') # si volem obrir el fitxer
+#  ds = xr.open_dataset('stats.nc', engine ='netcdf4') # si volem obrir el fitxer
 
-#print("Stats created successfully.")
+print("Stats created successfully.")
 
     # A partir de previous_matches faria el recompte de gols anotats per cada jugador i estadístiques similars.
     # ELO s'ha de calcular per cada partit inidividualment, perquè es vagi actualitzant
     #print(previous_matches.groupby('Guanyador').count())
-
-
