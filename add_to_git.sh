dcadcf5f8ac2bdf4b545aa6d435b73118c35fd6f
PGT <paugrebol@gmail.com>
2026-05-19 15:16:22 +0200

    Updated with filenames

--- add_to_git.sh
+++ add_to_git.sh
@@ -2,10 +2,12 @@
 
 # Add to git
 git add results/Season*/frequencies_Season_*.png
-git add results/Season*/ELO_stats_season_*.png
+git add results/Season*/ELO_stats_Season_*.png
+git add results/Season*/scatter_plotsSeason_*.png
 git add results/standings.md
 git add results/winplayed_stats.html
-git add results/Season*winplayed_stats_season_*.png
+git add results/Season*/winplayed_statsSeason_*.html
+git add results/Season*/winplayed_stats_Season_*.png
 git add generated_files/results_Season_*.csv
 git add generated_files/stats_Season_*.nc
 git add generated_files/teammates_Season_*.nc
